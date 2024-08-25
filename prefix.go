3592ef408e5152d22cb7efdd6df6cc619748749d
Matúš Ollah <matusollah420@gmail.com>
2024-08-25 17:26:37 +0200

    comment Prefix

--- prefix.go
+++ prefix.go
@@ -4,6 +4,7 @@ import (
 	"github.com/fatih/color"
 )
 
+// Prefix prepends a colored prefix to msg.
 func Prefix(prefix string, msg string) string {
 	return color.New(color.BgHiWhite, color.FgBlack).Sprint(prefix) + " " + msg
 }
