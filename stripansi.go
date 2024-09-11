521bd4b023c130100b2f7dc8a8f23ded9f98a19e
Matúš Ollah <matusollah420@gmail.com>
2024-09-11 13:32:03 +0200

    fix regexp

--- stripansi.go
+++ stripansi.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-var re = regexp.MustCompile(`[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))`)
+var re = regexp.MustCompile("[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))")
 
 // stripANSI removes ANSI escape sequences from the provided bytes.Buffer.
 func stripANSI(bf *bytes.Buffer) {
