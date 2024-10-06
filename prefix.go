09012af7a05d2c511033c35706d6ae56b2f3ff3d
Matúš Ollah <matusollah420@gmail.com>
2024-10-06 12:12:20 +0200

    improve Prefix function

--- prefix.go
+++ prefix.go
@@ -1,10 +1,16 @@
 package slogcolor
 
 import (
+	"strings"
+
 	"github.com/fatih/color"
 )
 
 // Prefix prepends a colored prefix to msg.
-func Prefix(prefix string, msg string) string {
-	return color.New(color.BgHiWhite, color.FgBlack).Sprint(prefix) + " " + msg
+func Prefix(prefix string, msg ...string) string {
+	if len(msg) == 0 {
+		return color.New(color.BgHiWhite, color.FgBlack).Sprint(prefix)
+	}
+
+	return color.New(color.BgHiWhite, color.FgBlack).Sprint(prefix) + " " + strings.Join(msg, " ")
 }
